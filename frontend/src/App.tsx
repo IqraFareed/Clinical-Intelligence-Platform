7d25a54530317223d2b9f21b43f5e1120e47c72b
Iqrafareed <iqrafareed56@gmail.com>
2026-05-20 23:05:12 -0500

    routing configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashoboard";
+import Patients from "./pages/Patient";
+import Visits from "./pages/Visits";
+import Analytics from "./pages/Analytics";
 const App = () => {
-  return <div className="bg-red-400">App</div>;
+  return (
+    <BrowserRouter>
+      <div className="p-6">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/patients" element={<Patients />} />
+          <Route path="/visits" element={<Visits />} />
+          <Route path="/analytics" element={<Analytics />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
